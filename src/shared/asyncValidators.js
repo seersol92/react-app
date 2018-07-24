7f1cb78ad1fa3782d917137a17dc6c26a945df17
seersol92 <hamad.pixiders@gmail.com>
2018-07-24 18:31:25 +0500

    userName in async validators

--- src/shared/asyncValidators.js
+++ src/shared/asyncValidators.js
@@ -2,9 +2,10 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const asyncValidate = (values /*, dispatch */) => {
   return sleep(1000).then(() => {
+    
     // simulate server latency
-    if (['john', 'paul', 'george', 'ringo'].includes(values.username)) {
-      throw { username: 'That username is taken' }
+    if (['john', 'paul', 'george', 'ringo'].includes(values.userName)) {
+      throw { userName: 'That username is taken' }
     }
   })
 }
